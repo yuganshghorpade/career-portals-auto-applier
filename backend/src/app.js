c070ecb60870ddb5580a801d8fdee8b6dc67396d
Yugansh Ghorpade <yuganshghorpade2005@gmail.com>
2025-09-21 22:47:50 +0530

    fix: Ensure CORS configuration allows credentials for specific origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,7 @@ import glassdoorRoutes from './routes/glassdoor.js';
 const app = express();
 app.use(cors({
     origin:'https://career-portals-auto-applier.vercel.app',
+    credentials:true
 }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
