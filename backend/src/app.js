a5132103363eb0038411b40b4e29f11d3333e809
Yugansh Ghorpade <yuganshghorpade2005@gmail.com>
2025-09-21 20:15:51 +0530

    fix: Update CORS configuration to restrict access to specific origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import glassdoorRoutes from './routes/glassdoor.js';
 
 const app = express();
 app.use(cors({
-    origin:'*',
+    origin:'https://career-portals-auto-applier.vercel.app',
 }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
